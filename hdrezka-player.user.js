cba2f2d013017311ea7f7b4e87207ab0b8120a19
Chilli <chillirf@gmail.com>
2026-03-09 08:36:35 +0300

    Universal selector (include preloader)

--- hdrezka-player.user.js
+++ hdrezka-player.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         hdrezka player
 // @namespace    http://top-kek.ml/
-// @version      0.1.1
+// @version      0.1.2
 // @description  try to take over the world!
 // @author       Chilli
 // @match        https://rezka.ag/*
@@ -14,8 +14,7 @@ GM_addStyle(`
         padding-right: 0;
     }
 
-    div#cdnplayer-container,
-    div#cdnplayer {
+    div[id^="cdnplayer"] {
         width: 100% !important;
         height: auto !important;
         aspect-ratio: 16 / 9 !important;
