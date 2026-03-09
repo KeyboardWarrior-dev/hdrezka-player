e7ddf1c4c66970434d73f580798eef481d54af84
Chilli <chillirf@gmail.com>
2026-03-09 08:20:54 +0300

    Trim whitespaces, fix googlecast button pos

--- hdrezka-player.user.js
+++ hdrezka-player.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         hdrezka player
 // @namespace    http://top-kek.ml/
-// @version      0.1.0
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       Chilli
 // @match        https://rezka.ag/*
@@ -13,12 +13,16 @@ GM_addStyle(`
     div.b-content__columns {
         padding-right: 0;
     }
-    
-    div#cdnplayer-container, 
+
+    div#cdnplayer-container,
     div#cdnplayer {
         width: 100% !important;
         height: auto !important;
         aspect-ratio: 16 / 9 !important;
+    }
+
+    pjsdiv:has(> pjsdiv > button#pjs_cast_button_cdnplayer) {
+        top: 20px !important;
     }
 `);
 
