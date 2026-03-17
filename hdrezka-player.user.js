804a5e6e2adc61b8161a401773c22689913d2965
Chilli <chillirf@gmail.com>
2026-03-17 08:59:51 +0300

    CSS update, match domains update

--- hdrezka-player.user.js
+++ hdrezka-player.user.js
@@ -1,15 +1,29 @@
 // ==UserScript==
 // @name         hdrezka player
 // @namespace    http://top-kek.ml/
-// @version      0.1.2
+// @version      0.1.3
 // @description  try to take over the world!
 // @author       Chilli
 // @match        https://rezka.ag/*
+// @match        https://hdrezka.tv/*
+// @match        https://standby-rezka.tv/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        GM_addStyle
 // ==/UserScript==
 
 GM_addStyle(`
+    li.b-translator__item {
+        min-width: 232.7px !important;
+    }
+
+    li.b-simple_season__item {
+        min-width: 69.1px !important;
+    }
+
+    li.b-simple_episode__item {
+        min-width: 69.1px !important;
+    }
+
     div.b-content__columns {
         padding-right: 0;
     }
